89bcd75241eb21566896f315d42ebaccfef42433
Ondřej Ježek <xjezek2@fi.muni.cz>
2022-10-27 19:29:05 +0200

    javadoc for SampleUsage

--- src/main/java/cz/muni/fi/pv168/seminar01/beta/Data/SampleUsage.java
+++ src/main/java/cz/muni/fi/pv168/seminar01/beta/Data/SampleUsage.java
@@ -7,12 +7,25 @@ import cz.muni.fi.pv168.seminar01.beta.Model.Vehicle;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Class for example of usage of TestDataGenerator.
+ *
+ */
 public final class SampleUsage {
     private final TestDataGenerator testDataGenerator = new TestDataGenerator();
     private final List<Vehicle> vehicles = new ArrayList<>();
     private final List<Passenger> passengers = new ArrayList<>();
     private final List<Ride> rides = new ArrayList<>();
 
+    /**
+     * This method generates sample data.
+     * 10 passengers
+     * 4 vehicles
+     * 35 rides
+     *
+     * It's also an example of how to generate data with testDataGenerator.
+     */
     public void generateSampleData() {
         for (int i = 0; i < 10; i++) {
             passengers.add(testDataGenerator.createPassenger());
