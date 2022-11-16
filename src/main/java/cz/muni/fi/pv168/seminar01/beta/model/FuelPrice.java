ad48f89c2faf351caebe910a4c94fbd39221893d
Jakub Ratajik <xratajik@fi.muni.cz>
2022-11-16 14:56:31 +0100

    make getFuelPrice return 0 by default

--- src/main/java/cz/muni/fi/pv168/seminar01/beta/model/FuelPrice.java
+++ src/main/java/cz/muni/fi/pv168/seminar01/beta/model/FuelPrice.java
@@ -7,10 +7,8 @@ public class FuelPrice {
     private static BigDecimal electricityPrice = new BigDecimal("7.2");
     private static BigDecimal LPGPrice = new BigDecimal("21.3");
     private static BigDecimal dieselPrice = new BigDecimal("49.7");
-    private static BigDecimal randomPrice = new BigDecimal("43.2");
 
-    private FuelPrice() {
-    }
+    private FuelPrice() {}
 
     public static BigDecimal getFuelPrice(FuelType fuelType) {
         return switch (fuelType) {
@@ -18,7 +16,7 @@ public class FuelPrice {
             case DIESEL -> dieselPrice;
             case GASOLINE -> gasolinePrice;
             case ELECTRICITY -> electricityPrice;
-            default -> randomPrice;
+            default -> BigDecimal.ZERO;
         };
     }
 
