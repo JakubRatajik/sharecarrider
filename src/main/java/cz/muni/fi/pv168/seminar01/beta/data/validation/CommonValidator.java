ff33d6bfc242ff05b4122dec13dcc735bcf3c743
Jakub Ratajik <xratajik@fi.muni.cz>
2022-11-19 18:40:59 +0100

    created CommonValidator class

---
+++ src/main/java/cz/muni/fi/pv168/seminar01/beta/data/validation/CommonValidator.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pv168.seminar01.beta.data.validation;
+
+/**
+ * @author Jakub Ratajik
+ */
+public class CommonValidator {
+    private CommonValidator() {}
+
+    public static boolean isValidLongAlphaSpaceString(String string) {
+        return string.matches("^[a-zA-Z ]{1,100}$");
+    }
+
+
+}
