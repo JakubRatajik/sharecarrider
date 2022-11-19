9eb36eb793015098a95a17ec44d47efe4dd7bd12
Jakub Ratajik <xratajik@fi.muni.cz>
2022-11-20 00:10:14 +0100

    extend CommonValidator methods

--- src/main/java/cz/muni/fi/pv168/seminar01/beta/data/validation/CommonValidator.java
+++ src/main/java/cz/muni/fi/pv168/seminar01/beta/data/validation/CommonValidator.java
@@ -1,5 +1,10 @@
 package cz.muni.fi.pv168.seminar01.beta.data.validation;
 
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
 /**
  * @author Jakub Ratajik
  */
@@ -11,5 +16,53 @@ public class CommonValidator {
         return string.matches("^[a-zA-Z ]{1,100}$");
     }
 
+    public static boolean isValidDateParsing(String date) {
+        try {
+            LocalDate.parse(date.trim());
+            return true;
+        }
+        catch (DateTimeParseException e) {
+            return false;
+        }
+    }
 
+    public static boolean isValidIntParsing(String num) {
+        try {
+            Integer.parseInt(num.trim());
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidFloatParsing(String num) {
+        try {
+            Float.parseFloat(num.trim());
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidTimeParsing(String time) {
+        try {
+            LocalTime.parse(time.trim());
+            return true;
+        }
+        catch (DateTimeParseException e) {
+            return false;
+        }
+    }
+
+    public static boolean isValidLongParsing(String num) {
+        try {
+            Long.parseLong(num.trim());
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
