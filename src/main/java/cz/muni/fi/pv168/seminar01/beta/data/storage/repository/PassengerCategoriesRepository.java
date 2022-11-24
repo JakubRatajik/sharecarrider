1d990d278b2d59aed220123ffc93a25fb1995ba0
Jan Maceček <xmacece1@fi.muni.cz>
2022-11-24 19:43:41 +0100

    repository0.1

--- src/main/java/cz/muni/fi/pv168/seminar01/beta/data/storage/repository/PassengerCategoriesRepository.java
+++ src/main/java/cz/muni/fi/pv168/seminar01/beta/data/storage/repository/PassengerCategoriesRepository.java
@@ -1,7 +1,64 @@
 package cz.muni.fi.pv168.seminar01.beta.data.storage.repository;
 
+import cz.muni.fi.pv168.seminar01.beta.model.PassengerCategory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * @author Jan Macecek
  */
-public class PassengerCategoriesRepository {
+public class PassengerCategoriesRepository implements Repository<PassengerCategory> {
+
+    private List<PassengerCategory> passengerCategories = new ArrayList<>();
+
+    public PassengerCategoriesRepository() {
+
+        this.refresh();
+    }
+    @Override
+    public int getSize() {
+        return passengerCategories.size();
+    }
+
+    @Override
+    public Optional<PassengerCategory> findById(long id) {
+        return passengerCategories.stream().filter(e -> e.getId() == id).findFirst();
+    }
+
+    @Override
+    public Optional<PassengerCategory> findByIndex(int index) {
+        if (index < getSize())
+            return Optional.of(passengerCategories.get(index));
+        return Optional.empty();
+    }
+
+    @Override
+    public List<PassengerCategory> findAll() {
+        return Collections.unmodifiableList(passengerCategories);
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+
+    @Override
+    public void create(PassengerCategory newEntity) {
+        //--TODO db functionality add
+        passengerCategories.add(newEntity);
+    }
+
+    @Override
+    public void update(PassengerCategory entity) {
+
+    }
+
+    @Override
+    public void deleteByIndex(int index) {
+        //--TODO connect this to DAO
+        findByIndex(index).ifPresent(x -> passengerCategories.remove(x));
+    }
 }
